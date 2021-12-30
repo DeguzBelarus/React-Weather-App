1adbef4ed5b29b4c6ab37e35ddb60837ad1775ec
Anton <deguz@mail.ru>
2021-12-30 15:50:47 +0300

    fixes and improvements 4

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -18,27 +18,25 @@ const Main = ({ weather, units }) => {
       <p className="city-name-paragraph">
         Город:
         <span className="city-name">
-          {weather?.name ? ` ${weather.name}` : ` Город не выбран`}
+          {weather?.main ? ` ${weather.name}` : ` Город не выбран`}
         </span>
       </p>
       <p className="city-temp-paragraph">
         Температура:
         <span className="city-temp">
-          {weather?.main?.temp ? ` ${weather.main.temp} ${measurement}` : ``}
+          {weather?.main ? ` ${weather.main.temp} ${measurement}` : ``}
         </span>
       </p>
       <p className="additionalinfo-paragraph">
         Влажность:
         <span className="additional-info">
-          {weather?.main?.humidity ? ` ${weather.main.humidity}%` : ``}
+          {weather?.main ? ` ${weather.main.humidity}%` : ``}
         </span>
       </p>
       <p className="additionalinfo-paragraph">
         Описание:
         <span className="additional-info">
-          {weather?.weather[0]?.description
-            ? ` ${weather.weather[0].description}`
-            : ``}
+          {weather?.main ? ` ${weather.weather[0].description}` : ``}
         </span>
       </p>
     </main>
